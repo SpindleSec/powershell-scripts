b18c8ce8992dfd500b01064e31e7a7a1eefe95cf
Spindle Security <114083317+SpindleSec@users.noreply.github.com>
2023-01-13 16:29:05 +0000

    Create Get-SpfTextRecordCount.ps1

---
+++ Get-SpfTextRecordCount.ps1
@@ -0,0 +1,41 @@
+function Get-SPFTxtRecordCount {
+    param (
+        [string] $DomainName,
+		[bool] $Loud = $false
+    )
+
+	if($Loud) {Write-Host "Finding record count for domain $DomainName"}
+
+	$count = 0;
+
+	$txtRecords = Resolve-DnsName -Type TXT -Name $DomainName -Server "8.8.8.8"
+	foreach($txt in $txtRecords) {
+		if($($txt.Strings) -like "v=spf1*") {
+			foreach ($SpfEntry in $($txt.Strings.Split(" ").Trim())) {
+				if($SpfEntry -like "ip4:*") {
+					$EntryCount = 0;
+					if($Loud) {Write-Host "Finding record count for subnet $SpfEntry for $DomainName"}
+
+					if($SpfEntry -like "*/*") {
+						$EntryCount = [Math]::Pow(2, 32 - ($SpfEntry.Substring(([int] $SpfEntry.IndexOf("/")) + 1).Trim()))
+
+						if($Loud) {Write-Host "Found $EntryCount records for subnet $SpfEntry for $DomainName"}
+						$count += $EntryCount
+					}
+					else {
+						if($Loud) {Write-Host "Found 1 record for subnet $SpfEntry for $DomainName"}
+						$count += 1
+					}
+				}
+				if($SpfEntry -like "include:*") {
+					$domain = $SpfEntry.Substring(([int] $SpfEntry.IndexOf(":")) + 1).Trim()
+					$count += Get-SPFTxtRecordCount $domain
+				}
+			}
+		}
+	}
+
+	if($Loud) {Write-Host "Found $count records for domain $DomainName"}
+
+	return $count
+}
